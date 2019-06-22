802af4d265476d155829ed87f321ac8908fc6793
AIちゃん <sakaku193@gmail.com>
2019-06-22 23:41:09 +0900

    値飛ばしてみた。

--- 03form_get/main.js
+++ 03form_get/main.js
@@ -1,26 +1,32 @@
 let http = require("http");
 let fs = require("fs");
+let url = require("url");
+let qs = require("querystring");
+
+let indexPage = fs.readFileSync("./html/index.html","utf-8");
 
 let server = http.createServer(function(req, res){
-    let target = "";
-    switch(req.url){
-      case "/":
-      case "/index":
-        target = "./html/index.html";
-        break;
-      case "/next":
-        target = "./html/next.html";
-        break;
-      default:
-        res.writeHead(404, {"Content-Type":"text/plain"});
-        res.end("bad request");
-        return;
-    }
-    fs.readFile(target, "utf-8", function(err, data){
-      res.writeHead(200, {"Content-Type":"text/html"});
-      res.write(data);
-      res.end();
+  if (req.method == "GET") {
+    let urlParts = url.parse(req.url, true);
+    console.log("GET Request");
+    console.log("name = " + urlParts.query.name);
+    console.log("age = " + urlParts.query.age);
+  }else{
+    let body = "";
+    req.on("data", function(data){
+      body = body + data;
     });
+    req.on("end", function(){
+      let params= qs.parse(body);
+      console.log("POST Request");
+      console.log("name = " + params.name);
+      console.log("age = " + params.age);
+    });
+  }
+
+  res.writeHead(200, {"Content-Type":"text/html"});
+  res.write(indexPage);
+  res.end();
 });
 
 server.listen(1234);
